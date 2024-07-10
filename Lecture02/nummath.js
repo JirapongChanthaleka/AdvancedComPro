28a42bfb9af50e9514223f0b897fc816b49a87cf
JirapongChanthaleka <s6506022620061@email.kmutnb.ac.th>
2024-07-10 09:21:20 +0700

    update L02

---
+++ Lecture02/nummath.js
@@ -0,0 +1,19 @@
+let num1 = 100;
+
+//Basic Math
+console.log("********Basic Math********");
+console.log(num1 + 25);
+console.log(num1 - 100);
+console.log(num1 * 100);
+console.log(num1 / 1500);
+
+console.log("********Additional MAthematical Operations********");
+//Additional Mathematical Operations
+console.log(num1 % 1500); // Remainder
+console.log(++num1); //Increment
+console.log(--num1); //Decrement
+
+console.log("********Using the MAth Object********");
+//Using the Math Object
+console.log(Math.PI); //Pi
+console.log(Math.sqrt(num1)); //Squre root
