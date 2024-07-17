fcdb2a1e29044e643c48b4328c3069523dba0fe3
JirapongChanthaleka <s6506022620061@email.kmutnb.ac.th>
2024-07-17 11:06:31 +0700

    update L03

---
+++ Lecture03/array_practice2.js
@@ -0,0 +1,8 @@
+let furniture = ['Table', 'Chairs', 'Couch'];
+
+for (let fer of furniture) {
+    console.log(`Letters in "${fer}":`);
+    for (let letter of fer) {
+        console.log(letter);
+    }
+}
