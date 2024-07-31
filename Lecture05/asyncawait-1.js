250977b7c85226f6b763868d26a87951766427c0
JirapongChanthaleka <s6506022620061@email.kmutnb.ac.th>
2024-07-31 09:01:23 +0700

    update L05

--- Lecture05/asyncawait-1.js
+++ Lecture05/asyncawait-1.js
@@ -4,19 +4,11 @@ function promiseTimeout(ms) {
     });
 }
 
-async function longRunningOperation() {
-    // logic
-    return 42;
-}
-
 async function run() {
     // logic
     console.log("Start!!");
+    // try take of await and compare
     await promiseTimeout(2000);
-    //try to take await out and see
-    const response =  longRunningOperation();
-    console.log(response);
-
     console.log("Stop!!");
 }
 
